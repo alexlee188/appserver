e50ab5c708b4afe1f6fd334691ba9d2da0e113e4
Alex Lee <alexlee188@gmail.com>
2014-06-27 12:17:22 +0800

    SGnurse: SSL TCP connection working

--- android/src/com/alexlee188/sgnurse/TCPClient.java
+++ android/src/com/alexlee188/sgnurse/TCPClient.java
@@ -158,7 +158,8 @@ public class TCPClient {
                 out = new BufferedWriter(new OutputStreamWriter(socket.getOutputStream()));
                 Log.e("TCP Client", "BufferedWriter on OutputStream of socket created.");
  
-                sendMessage("test");
+                sendMessage("0065<?xml version=\"1.0\" encoding=\"UTF-8\"?> "
+                		+ "<QUERY>GetJobs</QUERY>");
                 //receive the message which the server sends back
                 in = new BufferedReader(new InputStreamReader(socket.getInputStream()));
  
@@ -197,8 +198,6 @@ public class TCPClient {
                 //the socket must be closed. It is not possible to reconnect to this socket
                 // after it is closed, which means a new socket instance has to be created.
                 socket.close();
-                in.close();
-                out.close();
             } // end try
             
             } // end if secured
