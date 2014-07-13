7df8f05fdab24ddfb736dbcdce5e44de631994b3
Alex Lee <alexlee188@gmail.com>
2014-07-14 03:51:31 +0800

    SGnurse: added fragment_assigned layout

--- JOB.sql
+++ JOB.sql
@@ -43,7 +43,7 @@ CREATE TABLE `JOB` (
 
 LOCK TABLES `JOB` WRITE;
 /*!40000 ALTER TABLE `JOB` DISABLE KEYS */;
-INSERT INTO `JOB` VALUES (1,1,'Change of leg dressing','Wound care',NULL,NULL,'2014-11-10 18:00:00',2.5,'assigned',7),(2,2,'General','Geriatrics',NULL,NULL,'2014-09-30 12:00:00',1,'assigned',8),(3,1,'General',NULL,NULL,NULL,'2014-10-10 18:00:00',1.5,'open',NULL),(4,3,'Bathing and Turning','Geriatrics',NULL,NULL,'2014-09-28 12:00:00',1,'open',NULL),(5,4,'Ryles tube feeding',NULL,NULL,NULL,'2014-11-13 14:00:00',1,'open',NULL),(6,3,'Ryles tube feeding',NULL,NULL,NULL,'2014-11-15 18:00:00',1,'assigned',4),(7,2,'Bed sore dressing','Wound care','Geriatrics',NULL,'2014-12-18 18:00:00',2,'open',NULL),(8,2,'Turning and bathing','Geriatrics',NULL,NULL,'2014-10-10 14:00:00',3.5,'assigned',2);
+INSERT INTO `JOB` VALUES (1,1,'Change of leg dressing','Wound care',NULL,NULL,'2014-11-10 18:00:00',2.5,'assigned',7),(2,2,'General','Geriatrics',NULL,NULL,'2014-09-30 12:00:00',1,'assigned',8),(3,1,'General',NULL,NULL,NULL,'2014-10-10 18:00:00',1.5,'open',NULL),(4,3,'Bathing and Turning','Geriatrics',NULL,NULL,'2014-09-28 12:00:00',1,'open',NULL),(5,4,'Ryles tube feeding',NULL,NULL,NULL,'2014-11-13 14:00:00',1,'assigned',5),(6,3,'Ryles tube feeding',NULL,NULL,NULL,'2014-11-15 18:00:00',1,'assigned',4),(7,2,'Bed sore dressing','Wound care','Geriatrics',NULL,'2014-12-18 18:00:00',2,'open',NULL),(8,2,'Turning and bathing','Geriatrics',NULL,NULL,'2014-10-10 14:00:00',3.5,'assigned',2);
 /*!40000 ALTER TABLE `JOB` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -56,4 +56,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2014-07-12 21:42:58
+-- Dump completed on 2014-07-13 13:30:53
