11a5b60046391c2c2cf5701053debc29aac60c65
Alex Lee <alexlee188@gmail.com>
2015-03-08 15:11:45 +0800

    appserver: return -1 if any of the transactions fails

--- db_functions.c
+++ db_functions.c
@@ -117,6 +117,17 @@ int assign_job_to_user(char* job_id, char* gcm_regid){
     		finish_with_warning(con);
 		return -1;
     	}
+        if (mysql_affected_rows(con) != 1) 
+        {
+	    strcpy(buf, "rollback;");
+    	    if (mysql_query(con, buf)) {      
+    		finish_with_warning(con);
+		return -1;
+    	    }
+            finish_with_warning(con);
+	    return -1;
+        }
+
 	strcpy(buf, "update JOB set JOB_STATUS = 'requested', JOB_ASSIGNED_ID = (select id from gcm_users where gcm_regid = '");
 	strcat(buf, gcm_regid);
 	strcat(buf, "') where JOB_ID ='");
@@ -126,20 +137,25 @@ int assign_job_to_user(char* job_id, char* gcm_regid){
     		finish_with_warning(con);
 		return -1;
     	}
-	strcpy(buf, "commit;");
+        if (mysql_affected_rows(con) != 1) 
+        {
+	    strcpy(buf, "rollback;");
+    	    if (mysql_query(con, buf)) {      
+    		finish_with_warning(con);
+		return -1;
+    	    }
+            finish_with_warning(con);
+	    return -1;
+        }
 
+	strcpy(buf, "commit;");
     	if (mysql_query(con, buf)) {      
     		finish_with_warning(con);
 		return -1;
     	}
 
-/*
-        if (mysql_affected_rows(con) != 1) 
-        {
-            finish_with_warning(con);
-	    return -1;
-        }
-*/
+
+
 	return 0;  // success
 }
 
