ae70633e9c6353beda2a5ae47442dd64040b7770
Alex Lee <alexlee188@gmail.com>
2015-03-25 08:38:39 +0800

    use ssh tunnel for gcm_server access

--- gcm_server_php/config.php
+++ gcm_server_php/config.php
@@ -2,7 +2,7 @@
 /**
  * Database config variables
  */
-define("DB_HOST", "localhost");
+define("DB_HOST", "localhost:3307");
 define("DB_USER", "gcm_user");
 define("DB_PASSWORD", "gcm_user188");
 define("DB_DATABASE", "gcm");
