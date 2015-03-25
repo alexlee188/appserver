fd0181bd61f383fac09f8a79681b4386dafa7b44
Alex Lee <alexlee188@gmail.com>
2015-03-25 16:45:44 +0800

    gcm_server_php: added comment of RDB host info

--- gcm_server_php/config.php
+++ gcm_server_php/config.php
@@ -2,6 +2,10 @@
 /**
  * Database config variables
  */
+
+// To connect direct to RDB host
+// define("DB_HOST", "gcmdbinstance.cegfhjvyp8lf.ap-southeast-1.rds.amazonaws.com");
+// To connect through ssh tunnel
 define("DB_HOST", "127.0.0.1:3307");
 define("DB_USER", "gcm_user");
 define("DB_PASSWORD", "gcm_user188");
